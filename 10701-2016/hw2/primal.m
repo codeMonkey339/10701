aee81160a654ac4f2203c7ddd8af736a7108e43c
Justin <yang_yo@worksap.co.jp>
2018-05-05 20:11:18 +0800

    implemented the primal and test file. Need to run end-to-end test of the code with real data

--- 10701-2016/hw2/primal.m
+++ 10701-2016/hw2/primal.m
@@ -6,15 +6,27 @@ function [w, b, obj_val] = primal(X, Y)
 % b: the offset term
 % obj_val: the optimal objective value
 
+C = 1; % the regularization coefficient
 X = [X ones(size(X, 1), 1)];
 H = eye(size(X, 2));
 H = [H; zeros(size(X, 1), size(X, 2))];
 H_zero = zeros(size(X, 1), size(X, 1));
 H_zero = [zeros(size(X, 2), size(X, 1)); H_zero];
 H = [H H_zero];
-A = zeros(size(X, 1), size(X, 2));
+f = zero(size(X, 2), 1);
+f = [f; C * ones(size(X, 1), 1)]; 
+A1 = zeros(size(X, 1), size(X, 2));
 A_nonzero = -1 * ones(size(X, 1), size(X, 1));
-A = [A A_nonzero];
-b = zeros(size(X, 1), 1);
-% todo: need to complete the row-wise multiplication
+A1 = [A1 A_nonzero];
+b1 = zeros(size(X, 1), 1);
+A2 = -1 * bsxfun(@times, X, Y);
+A2 = [A2 -1 * ones(size(X, 1), size(X, 1))];
+b2 = ones(size(X, 1), 1);
+A = [A1; A2];
+b = [b1; b2];
+[w, fval, exitflag, output] = quadprog(H, f, A, b);
+b = w(length(w));
+w = w(1:lenght(w) - 1);
+
+end
 
