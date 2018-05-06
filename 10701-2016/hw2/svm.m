159d2a885c44aedb195cf3f996411ed6bd6f85f7
Justin <justinyang339@yahoo.com>
2018-05-07 01:38:14 +0800

    debug primal.m, need to consider how is beta0 learned

--- 10701-2016/hw2/svm.m
+++ 10701-2016/hw2/svm.m
@@ -1,13 +1,16 @@
 load ('./news.mat');
 %pkg load optim
 
-% todo: need to replace the name of variables here and check dims
 % X_train = standardize(X_train);
+
+% quad programming for primal problem
+[w2, b2, fval2] = primal(X_train, y_train');
+Ypred_primal = test(X_test, w2, b2);
+test_accuracy_primal = sum(Ypred_primal == y_test) / length(y_test);
+
+% quad programming for dual problem
 [w1, b1, fval1] = dual(X_train, y_train');
 Ytest_dual = test(X_test,w1, b1);
 Ytrain_dual = test(X_train, w1, b1);
 test_accuracy_dual = sum(Ytest_dual == y_test) / length(y_test);
 train_accuracy_dual = sum(Ytrain_dual == y_train) / length(y_train);
-%[w2, b2, fval2] = primal(X_train, y_train');
-%Ypred_primal = test(X_test, w2, b2);
-%test_accuracy_primal = sum(Ypred_primal == y_test) / length(y_test);
