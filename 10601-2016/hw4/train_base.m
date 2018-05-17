f30bd70f6ff03cd607115c217af5d9f69b6a94e5
Justin <justinyang339@yahoo.com>
2018-05-17 10:57:59 +0800

    implemented scan_axis which finds the best stump along an axis, and the tran_base method which trains a base classifier. Need to restore the order of data weights

--- 10601-2016/hw4/train_base.m
+++ 10601-2016/hw4/train_base.m
@@ -7,6 +7,32 @@ function [model] = train_base(XTrain, YTrain, weight)
 % model.weight n * 1 updated data weight
 % model.alpha weight of the classifier
 % model.dim dimension of the deicison variable
-% model.direction direction of the positive data
+% model.direction direction of the positive data, +1 for pos, -1 for neg
 
+maxWeight = 0;
+pos = 0;
+dir = 0;
+dim = 0;
+correctI = yTrain;
+I = yTrain;
+for i = 1: size(sortedX, 2)
+    curModel = scan_axis(sortedX, sortedY, weight, i);
+    if curModel.accWeight > maxWeight
+        pos = model.pos;
+        dir = model.dir;
+        dim = model.axis;
+        correctI = curModel.correctI;
+        I = curModel.I;
+    end
+end
+sortedWeight = weight(I);
+errorWeight = sortedWeight' * (1 - correctI);
+modelWeight = 0.5 * log((1 - errorWeight) / errorWeight);
+updatedDataWeight = sortedWeight .* (exp(-1 * modelWeight * correctI));
+updatedDataWeight = updatedDataWeight(I);
+%TODO: need to restore the order of weights
+        
+%%%%%%%%%%%%%%%%%%%%% scan the y-axis direction %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    
 end
